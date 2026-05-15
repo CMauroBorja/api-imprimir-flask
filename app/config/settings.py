cfa9e26d7c788efce0d48375b0d444d380bd4174
Carlos Mauricio Borja Mora <mauro.borja.mora@gmail.com>
2026-05-14 22:42:53 -0500

    Base arquitectura Flask modular

---
+++ app/config/settings.py
@@ -0,0 +1,16 @@
+import os
+
+class Config:
+    SERVER = os.getenv("SQL_SERVER", "localhost\\SQLEXPRESS")
+    DATABASE = os.getenv("SQL_DATABASE", "ElImperioDeLosBolsosBelen")
+    DRIVER = os.getenv("SQL_DRIVER", "ODBC Driver 17 for SQL Server")
+    USERNAME = os.getenv("SQL_USER", "sa")
+    PASSWORD = os.getenv("SQL_PASSWORD", "1234")
+    
+    SQLALCHEMY_DATABASE_URI = (
+        f"mssql+pyodbc://{USERNAME}:{PASSWORD}@"
+        f"{SERVER}/{DATABASE}"
+        f"?driver={DRIVER}&TrustServerCertificate=yes&charset=utf8"
+    )
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
