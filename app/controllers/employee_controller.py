ef58bdaa24e3af52c87139e83eee0dbf3aeb430e
Carlos Mauricio Borja Mora <mauro.borja.mora@gmail.com>
2026-05-19 20:26:24 -0500

    Migra createEmployee a employee controller

--- app/controllers/employee_controller.py
+++ app/controllers/employee_controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.models import Empleado
+from app.database.db import db
 
 employee_bp = Blueprint("employees", __name__)
 
@@ -17,3 +18,38 @@ def get_all_employees():
         }
         for e in empleados
     ])
+    
+@employee_bp.route("/createEmployee", methods=["POST"])
+def create_employee():
+    data = request.json
+    
+    required_fields = [
+        "nombre",
+        "telefono",
+        "codigo",
+        "contrasena",
+        "administrador"
+    ]
+    
+    if not data or any(field not in data for field in required_fields):
+        return jsonify({"error": "Faltan datos requeridos"}), 400
+    
+    empleado_existente = Empleado.query.filter_by(
+        codigo=data["codigo"].strip()
+    ).first()
+    
+    if empleado_existente:
+        return jsonify({"error": "El codigo de usuario ya existe"}), 409
+    
+    nuevo = Empleado(
+        nombre=data["nombre"].strip(),
+        telefono=data["telefono"].strip(),
+        codigo=data["codigo"].strip(),
+        contrasena=data["contrasena"].strip(),
+        administrador=data["administrador"]
+    )
+    
+    db.session.add(nuevo)
+    db.session.commit()
+    
+    return jsonify({"message": "Empleado creado exitosamente"}), 201
